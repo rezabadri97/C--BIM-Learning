496bc2325ed13acab6229b23469ff65ae483139e
Rezabadri97i <Rezabadri97@gmail.com>
2026-04-29 08:21:08 +0330

    Initial commit

---
+++ week_03/Day_03/Constructor/Constructor Overloading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Room
+{
+    public string Name { get; set; }
+    public double Area { get; set; }
+
+    public Room()
+    {
+        Name = "Unnamed Room";
+        Area = 0;
+    }
+
+    public Room(string roomName, double roomArea)
+    {
+        Name = roomName;
+        Area = roomArea;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Room room1 = new Room();
+        Room room2 = new Room("Office", 25.5);
+
+        Console.WriteLine("Room 1: " + room1.Name + ", " + room1.Area);
+        Console.WriteLine("Room 2: " + room2.Name + ", " + room2.Area);
+    }
+}
