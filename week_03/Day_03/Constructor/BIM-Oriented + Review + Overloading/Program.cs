4a8deaa921f5cf79c8e55e3c2265f85f1e8b2953
Rezabadri97i <Rezabadri97@gmail.com>
2026-04-29 09:38:07 +0330

    Add BIM element constructor overloading with validation

---
+++ week_03/Day_03/Constructor/BIM-Oriented + Review + Overloading/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+class BimElement
+{
+    public string Category;
+    private double height;
+    public double Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                height = value;
+            }
+            else
+            {
+                height = 1;
+            }
+        }
+    }
+    public BimElement()
+    {
+        Category = "Generic Moder";
+        Height = 1;
+    }
+    public BimElement(string c,double H)
+    {
+        Category = c;
+        Height = H;
+    }
+    
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        BimElement Element1=new BimElement();
+        Console.WriteLine(Element1.Category);
+        Console.WriteLine(Element1.Height);
+
+        BimElement Element2 = new BimElement("Wall", 3);
+        Console.WriteLine(Element2.Category);
+        Console.WriteLine(Element2.Height);
+    }
+}
