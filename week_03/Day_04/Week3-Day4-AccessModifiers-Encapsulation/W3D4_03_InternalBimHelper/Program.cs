5ed358cb7d37fd5e979dea6c0cd1539e76827906
Rezabadri97i <Rezabadri97@gmail.com>
2026-04-30 07:25:03 +0330

    Add internal BIM helper exercise

---
+++ week_03/Day_04/Week3-Day4-AccessModifiers-Encapsulation/W3D4_03_InternalBimHelper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+internal class BimParameterFormatter
+{
+    public string RawName(string name)
+    {
+        return name.ToUpper().Trim();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        BimParameterFormatter element1= new BimParameterFormatter();
+
+        
+        Console.WriteLine(element1.RawName(" door width "));
+        
+        
+    }
+}
