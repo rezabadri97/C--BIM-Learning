8faf1ffea3a2a18c3412054d6ef448498b642365
Rezabadri97i <Rezabadri97@gmail.com>
2026-04-30 06:53:36 +0330

    Add RevitRoom encapsulation exercise

--- week_03/Day_04/Week3-Day4-AccessModifiers-Encapsulation/W3D4_01_PublicPrivateBasics/Program.cs
+++ week_03/Day_04/Week3-Day4-AccessModifiers-Encapsulation/W3D4_01_PublicPrivateBasics/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class BimWall
+{
+    public string WallName;
+    private double height;
+
+    public void SetHeight(double value)
+    {
+        if (value > 0)
+        {
+            height = value;
+        }
+    }
+
+    public double GetHeight()
+    {
+        return height;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BimWall wall = new BimWall();
+
+        wall.WallName = "Core Wall";
+        wall.SetHeight(4.2);
+
+        Console.WriteLine("Wall Name: " + wall.WallName);
+        Console.WriteLine("Height: " + wall.GetHeight());
+    }
+}
---
+++ week_03/Day_04/Week3-Day4-AccessModifiers-Encapsulation/W3D4_02_RevitRoomEncapsulation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class RevitRoom
+{
+    public string RoomName {  get; set; }
+    private double area;
+
+    public void SetArea(double value)
+    {
+        if (value>0)
+        {
+            area = value;
+        }
+    }
+    public double GetArea()
+    {
+        return area;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        RevitRoom room=new RevitRoom();
+        room.RoomName = "Lobby";
+        room.SetArea(120.5);
+        Console.WriteLine($"Room Name: {room.RoomName}");
+        Console.WriteLine($"Room Area: {room.GetArea()}");
+
+    }
+}
