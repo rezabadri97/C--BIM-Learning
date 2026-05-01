c127eb0417bca8143c26dc64e2ba16d1dccebc4e
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-01 06:55:06 +0330

    Complete W3Review_01_BimCategoryReporter

---
+++ week_03/Day_04/W3Review/W3Review_01_BimCategoryReporter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Enter BIM category:");
+        string category = Console.ReadLine().ToLower().Trim();
+
+        switch (category)
+        {
+            case "wall":
+                Console.WriteLine("This is a vertical building element.");
+                break;
+            case "door":
+                Console.WriteLine("This element allows access between spaces.");
+                break;
+            case "window":
+                Console.WriteLine("This element provides light and visibility.");
+                break;
+            case "room":
+                Console.WriteLine("This element is used for spatial information.");
+                break;
+            case "":
+                Console.WriteLine("Please Enter the Category");
+                break;
+            default:
+                Console.WriteLine("Unknown BIM category.");
+                break;
+        }
+    }
+}
