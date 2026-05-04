0651c8627ace23185d102a250966d07440d34e05
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-04 11:56:44 +0330

    Complete W4D1_02_RoomNameListPractice

---
+++ week_04/Day1/W4D1_List,add_remove_find/W4D1_02_RoomNameListPractice/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> roomNames = new List<string>();
+        roomNames.Add("Lobby");
+        roomNames.Add("Office 101");
+        roomNames.Add("MEP Room");
+        roomNames.Add("Storage");
+        roomNames.Add("Conference Room");
+        Console.WriteLine("******************************");
+        Console.WriteLine();
+        Console.WriteLine("Original List Names:");
+        Console.WriteLine();
+        Console.WriteLine(roomNames);
+        Console.WriteLine();
+        Console.WriteLine("******************************");
+        Console.WriteLine();
+        Console.WriteLine("Clean List Names:");
+        Console.WriteLine();
+        cleanNames(roomNames);
+        Console.WriteLine();
+        Console.WriteLine("******************************");
+        Console.WriteLine();
+        bool removed = roomNames.Remove("Storage");
+        
+        string foundName = roomNames.Find(room => room.ToUpper().Contains("MEP"));
+        Console.WriteLine("Updeted List Names:");
+        cleanNames(roomNames);
+        Console.WriteLine();
+        Console.WriteLine($"Any Room Removed? {removed}");
+        Console.WriteLine();
+        if (foundName !=null)
+        {
+            Console.WriteLine($"Found MEP Room: {foundName}");
+        }
+        else
+        {
+            Console.WriteLine("No MEP Room Was Wound.");
+        }
+
+
+    }
+    static void cleanNames(List<string> roomNames)
+    {
+        int count = 0;
+        foreach(string room in roomNames)
+        
+        {
+            count++;
+            Console.WriteLine($"{count}-{room.Trim().ToUpper()}");
+            Console.WriteLine();
+        }
+        Console.WriteLine($"Room Counts: {count}");
+    }
+}
