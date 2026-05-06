2c47b9e492d5468d4ba70f4af86b69bbf5ee0a22
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-06 08:19:41 +0330

    Add practice 02 wall length array analyzer

---
+++ Month01/Projects/Practice01_RoomAreaEvaluator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace RoomAreaEvaluator
+{
+    class Room
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Room Name:");
+            string name=Console.ReadLine();
+
+            Console.WriteLine("Enter The Room Length:");
+            double length=double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter The Room Width:");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Room Information");
+            Console.WriteLine();
+            Console.WriteLine($"Room Name: {name} | Room Length: {length} | Room Width: {width}");
+            Console.WriteLine();
+            Area(length,width);
+
+
+
+
+
+        }
+        static void Area(double length, double width)
+        {
+            double area= length* width;
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine();
+            if (area<10)
+            {
+                Console.WriteLine("Small Room");
+            }
+            else if (25>=area && area>=10)
+            {
+                Console.WriteLine("Medium Room");
+            }
+            else
+            {
+                Console.WriteLine("Big Room");
+            }
+        }
+    }
+}
---
+++ Month01/Projects/Practice02_WallLengthArrayAnalyzer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Practice02_WallLengthArrayAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] lengths = new double[5];
+            for (int i=0; i< lengths.Length;  i++)
+            {
+                Console.WriteLine("Enter wall length " + (i + 1) + ":");
+                lengths[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Sum(lengths);
+            
+
+
+
+        }
+        static void Sum(double[] lengths)
+        {
+            double sum = 0;
+            double longest = lengths[0];
+            foreach(double length in lengths)
+            {
+                sum+= length;
+                if (length>longest)
+                {
+                    longest = length;
+                }
+            }
+            double average = sum / lengths.Length;
+
+            Console.WriteLine($"Wall Sum Length: {sum}");
+            Console.WriteLine();
+            Console.WriteLine($"Longest Wall: {longest}");
+            Console.WriteLine();
+            Console.WriteLine($"Wall Average Length: {average}");
+            Console.WriteLine();
+        }
+    }
+
+
+
+}
