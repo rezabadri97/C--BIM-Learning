d9de9e5a2ca2e60a869cb57d6064d742612322ab
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-08 07:31:51 +0330

    Add practice 12 virtual override element descriptions

---
+++ Month01/Projects/Practice12_Virtual and Override for Element Info/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+class BimElement
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public BimElement(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"Element Id: {Id}");
+        Console.WriteLine($"Element Name: {Name}");
+    }
+
+}
+class BimWall : BimElement
+{
+    public double Length { get; set; }
+
+    public BimWall(int id, string name, double length) : base(id, name)
+    {
+        Length = length;
+    }
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Element Id: {Id}");
+        Console.WriteLine($"Element Name: {Name}");
+        Console.WriteLine($"Wall Length: {Length}");
+        
+    }
+}
+class BimDoor : BimElement
+{
+    public double Width { get; set; }
+    public BimDoor(int id, string name, double width) : base(id, name)
+    {
+        Width = width;
+    }
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Element Id: {Id}");
+        Console.WriteLine($"Element Name: {Name}");
+        Console.WriteLine($"Wall Width: {Width}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<BimElement> elements = new List<BimElement>
+        {
+            new BimWall(101, "Exterior Wall", 5),
+            new BimWall(102, "Interior Wall", 3),
+            new BimWall(103, "Exterior facade Wall", 7),
+
+            new BimDoor(201, "External Door", .9),
+            new BimDoor(202, "Internal Door", 1),
+            new BimDoor(203, "External Iron Door", 1.2)
+        };
+
+        foreach (BimElement element in elements)
+        {
+            element.DisplayInfo();
+            Console.WriteLine("*******************************************");
+        }
+    }
+}
