16d69ecc05287e372e3c1a0c09b09d593d1d3fa1
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-10 06:07:25 +0330

    Add practice 4 printable collection manager

---
+++ Week05/Day01/W05_Interfaces + Generics + Exception Handling/Printable Collection Manager/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+interface IPrintable
+{
+    void Print();
+}
+
+class RoomReport : IPrintable
+{
+    private string name;
+    private double area;
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                name = value;
+            }
+        }
+    }
+    public double Area
+    {
+        get
+        {
+            return area;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                area = value;
+            }
+        }
+    }
+    public RoomReport(string name, double area)
+    {
+        Name = name;
+        Area = area;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Printed Room-----Room Name: {name} | Room Area: {area}");
+    }
+}
+class Wall : IPrintable
+{
+    private string name;
+    private double length;
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                name = value;
+            }
+        }
+    }
+    public double Length
+    {
+        get
+        {
+            return length;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                length = value;
+            }
+        }
+    }
+    public Wall(string name, double area)
+    {
+        Name = name;
+        Length = area;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Printed Wall-----Wall Name: {name} | Wall Length: {length}");
+    }
+}
+class Door : IPrintable
+{
+    private string name;
+    private double width;
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                name = value;
+            }
+        }
+    }
+    public double Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                width = value;
+            }
+        }
+    }
+    public Door(string name, double width)
+    {
+        Name = name;
+        Width = width;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Printed Door-----Door Name: {name} | Door Width: {width}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<IPrintable> Ilist = new List<IPrintable>();
+        Ilist.Add(new RoomReport("Living Room", 15));
+        Ilist.Add(new Door("Wooden", 2));
+        Ilist.Add(new Wall("Exterior", 3));
+
+        foreach(IPrintable item in Ilist)
+        {
+            item.Print();
+        }
+    
+        
+    }
+}
--- Week05/Day01/W05_Interfaces + Generics + Exception Handling/Savable Project Settings/Program.cs
+++ Week05/Day01/W05_Interfaces + Generics + Exception Handling/Savable Project Settings/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+interface IPrintable
+{
+    void Print();
+}
+interface ISavable
+{
+    void Save();
+}
+
+class RoomReport : IPrintable,ISavable
+{
+    private string name;
+    private double area;
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                name = value;
+            }
+        }
+    }
+    public double Area
+    {
+        get
+        {
+            return area;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                area = value;
+            }
+        }
+    }
+    public RoomReport(string name, double area)
+    {
+        Name = name;
+        Area = area;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Printed Room-----Room Name: {name} | Room Area: {area}");
+    }
+    public void Save()
+    {
+        Console.WriteLine($"Saved Room-----Room Name: {name} | Room Area: {area}");
+    }
+
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        RoomReport room = new RoomReport("Living Room", 15);
+        room.Print();
+        room.Save();
+    }
+}
