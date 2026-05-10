904c05a1aa54e8d651df6f9329beea7e15a6ac86
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-10 05:22:50 +0330

    Add practice 1 printable room report

---
+++ Week05/Day01/W05_Interfaces + Generics + Exception Handling/Printable Room Report/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+interface IPrintable
+{
+    void Print();
+}
+
+class RoomReport:IPrintable
+{
+
+
+
+    private string name;
+    private double area;
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if(!string.IsNullOrWhiteSpace(value))
+            {
+                name = value;
+            }
+        }
+    }
+    public double Area
+    {
+        get
+        {
+            return area;
+        }
+        set
+        {
+            if(value>0)
+            {
+                area = value;
+            }
+        }
+    }
+    public RoomReport(string name, double area)
+    {
+        Name = name;
+        Area = area;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Room NAme: {name} | Room Area: {area}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        RoomReport room = new RoomReport("Office",30);
+        room.Print();
+
+    }
+}
