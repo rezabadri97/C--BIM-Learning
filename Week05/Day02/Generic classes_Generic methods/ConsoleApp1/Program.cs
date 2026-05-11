10e30f7cc9ac6f83ce2fb6d5363adacccf9021dc
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-11 06:06:46 +0330

    Add Generic classes practice01

---
+++ Week05/Day02/Generic classes_Generic methods/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Box<T>
+{
+    public T Value;
+
+    public Box(T value)
+    {
+        Value = value;
+    }
+    public void ShowValue()
+    {
+        Console.WriteLine(Value);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Box<int> boxNumber = new Box<int>(23);
+        
+        Console.WriteLine("Box Number: "+boxNumber.Value);
+
+        Box<string> boxName = new Box<string>("Red");
+        Console.WriteLine("Box Name: " + boxName.Value);
+
+
+    }
+}
