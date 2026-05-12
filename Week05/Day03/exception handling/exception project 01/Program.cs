8346beb004b6ae7bc446f0be482e3c7ba5bb1b92
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-12 22:20:31 +0330

    add finally block to level number parsing

--- Week05/Day03/exception handling/exception project 01/Program.cs
+++ Week05/Day03/exception handling/exception project 01/Program.cs
@@ -1,21 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+
+public class Room
+{
+    public string Name;
+    public int Height;
+
+    public Room(string name, int height)
+    {
+        Name = name;
+        Height = height;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine("Room Name: " + Name);
+        Console.WriteLine("Room Height: " + Height + " mm");
+    }
+}
+
+public class Repository<T>
+{
+    private List<T> _items = new List<T>();
+
+    public void Add(T item)
+    {
+        _items.Add(item);
+    }
+
+    public List<T> GetAll()
+    {
+        return _items;
+    }
+
+    public int Count()
+    {
+        return _items.Count;
+    }
+}
 
 public class Program
 {
     public static void Main()
     {
-        Console.Write("Enter number of doors: ");
-        string input = Console.ReadLine();
+        Repository<Room> roomRepository = new Repository<Room>();
 
-        bool isValid = int.TryParse(input, out int doorCount);
+        Console.Write("Enter room name: ");
+        string roomName = Console.ReadLine();
 
-        if (isValid)
+        Console.Write("Enter room height in mm: ");
+        string inputHeight = Console.ReadLine();
+
+        bool success = int.TryParse(inputHeight, out int roomHeight);
+
+        if (!success)
         {
-            Console.WriteLine("Door count is: " + doorCount);
+            Console.WriteLine("Invalid height. Room was not created.");
+        }
+        else if (roomHeight <= 0)
+        {
+            Console.WriteLine("Height must be greater than zero.");
         }
         else
         {
-            Console.WriteLine("Invalid number. Please enter a valid integer.");
+            Room room = new Room(roomName, roomHeight);
+            roomRepository.Add(room);
+
+            Console.WriteLine("Room added successfully.");
+            Console.WriteLine("Room count: " + roomRepository.Count());
+
+            foreach (Room item in roomRepository.GetAll())
+            {
+                item.ShowInfo();
+            }
         }
     }
 }
