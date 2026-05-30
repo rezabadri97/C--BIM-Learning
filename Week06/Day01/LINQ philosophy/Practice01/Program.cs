5e35901258db102873e32d53329490735fed0cad
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-30 05:39:25 +0330

    LINQ philosophy Practice numbers bigger than 5

---
+++ Week06/Day01/LINQ philosophy/Practice01/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+List<int> numbers = new List<int>() { 2, 5, 7, 9, 1, 10 };
+var bigNumbers = numbers.Where(number => number > 5);
+foreach(var number in bigNumbers)
+{
+    Console.WriteLine(number);
+}
