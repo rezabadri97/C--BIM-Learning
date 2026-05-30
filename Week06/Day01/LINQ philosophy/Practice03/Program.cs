fb2857e795d8b1b7212bb08c6a5fc3c2921abd8a
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-30 06:27:50 +0330

    LINQ philosophy square numbers

---
+++ Week06/Day01/LINQ philosophy/Practice03/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+var result = numbers.Select(number => number * number);
+foreach(var item in result)
+{
+    Console.WriteLine(item);
+}
