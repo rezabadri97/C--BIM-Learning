79daebbbec9aa7cba86a106fc30010eb682bd44d
Rezabadri97i <Rezabadri97@gmail.com>
2026-05-30 06:46:14 +0330

    LINQ philosophy finding even numbers

---
+++ Week06/Day01/LINQ philosophy/Project04/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var evenNumbers = numbers.Where(number => number % 2 == 0).Select(number => number * 100);
+foreach(var item in evenNumbers)
+{
+    Console.WriteLine(item);
+}
