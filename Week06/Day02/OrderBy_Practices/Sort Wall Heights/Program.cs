83aead7cdd083a279800c6e7dc6439e00f6755f9
Rezabadri97i <Rezabadri97@gmail.com>
2026-06-01 07:12:00 +0330

    sort wall heights using OrderBy

---
+++ Week06/Day02/OrderBy_Practices/Sort Wall Heights/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<int> walls = new List<int> { 3000, 1500, 4500, 2500, 2000 };
+
+var newWalls = walls.Order();
+foreach(var wall in newWalls)
+{
+    Console.WriteLine(wall);
+}
