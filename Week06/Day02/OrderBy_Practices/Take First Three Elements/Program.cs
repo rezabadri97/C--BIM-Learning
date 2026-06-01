40022671b691711c5ebc1b413e9d746fc4771535
Rezabadri97i <Rezabadri97@gmail.com>
2026-06-01 09:57:52 +0330

    take first three element names using LINQ Take

---
+++ Week06/Day02/OrderBy_Practices/Take First Three Elements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> elements = new List<string> { "Wall A", "Door A", "Window A", "Column A", "Floor A" };
+        var newElements = elements.Take(3);
+
+        foreach(var element in newElements)
+        {
+            Console.WriteLine(element);
+        }
+
+
+    }
+
+}
