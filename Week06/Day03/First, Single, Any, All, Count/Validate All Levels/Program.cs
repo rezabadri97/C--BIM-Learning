ab9579c9fa5a516de57d2a675e67570a6ebd8813
Rezabadri97i <Rezabadri97@gmail.com>
2026-06-02 11:15:52 +0330

    validate all levels using LINQ All

---
+++ Week06/Day03/First, Single, Any, All, Count/Validate All Levels/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class LevelInfo
+{
+    public string Name { get; set; }
+    public double Elevation { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<LevelInfo> levels = new List<LevelInfo>
+        {
+            new LevelInfo { Name = "Ground Floor", Elevation = 0 },
+            new LevelInfo { Name = "Level 1", Elevation = 3000 },
+            new LevelInfo { Name = "Level 2", Elevation = 6000 }
+        };
+
+        bool allLevelsValid = levels.All(l => l.Elevation >= 0);
+
+        Console.WriteLine("All levels valid:");
+        Console.WriteLine(allLevelsValid);
+    }
+}
