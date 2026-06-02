7b32cf6d8164cd09184487452999c4b2c4aa4741
Rezabadri97i <Rezabadri97@gmail.com>
2026-06-02 11:17:43 +0330

    count valid tall walls using LINQ Count

---
+++ Week06/Day03/First, Single, Any, All, Count/Count Valid Walls/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class WallInfo
+{
+    public string Name { get; set; }
+    public double Height { get; set; }
+    public string LevelName { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<WallInfo> walls = new List<WallInfo>
+        {
+            new WallInfo { Name = "Wall A", Height = 2000, LevelName = "Level 1" },
+            new WallInfo { Name = "Wall B", Height = 3000, LevelName = "Level 1" },
+            new WallInfo { Name = "Wall C", Height = 3500, LevelName = "Level 2" },
+            new WallInfo { Name = "Wall D", Height = 2200, LevelName = "Level 2" }
+        };
+
+        int tallWallCount = walls.Count(w => w.Height > 2500);
+
+        Console.WriteLine("Tall wall count:");
+        Console.WriteLine(tallWallCount);
+    }
+}
