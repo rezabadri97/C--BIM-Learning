c2b671c7520ad61e8545af9da421466e4236634e
Rezabadri97i <Rezabadri97@gmail.com>
2026-06-02 09:20:18 +0330

    find first tall wall using LINQ First

---
+++ Week06/Day03/First, Single, Any, All, Count/Find First Tall Wall/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Wallinfo
+{
+    public string Name {  get; set; }
+    public double Hieght { get; set; }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Wallinfo> walls = new List<Wallinfo>
+        {
+            new Wallinfo{Name="Wall A",Hieght=2900},
+            new Wallinfo{Name="Wall B",Hieght=3000},
+            new Wallinfo{Name="Wall C",Hieght=3100},
+            new Wallinfo{Name="Wall D",Hieght=3200}
+        };
+
+        var firstWall = walls.First(w => w.Hieght > 3000);
+
+       Console.WriteLine(firstWall.Name);
+
+
+
+
+    }
+}
