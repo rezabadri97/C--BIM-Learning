2a4b43b30079a91baf925e9beca8ad5383812199
Rezabadri97i <Rezabadri97@gmail.com>
2026-06-07 07:04:09 +0330

    Day01_Exercise01_WallAreaCalculator

---
+++ Review01/Day01/Day01/Day01_Exercise01_WallAreaCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Wall Name");
+        string wallName=Console.ReadLine();
+        
+        Console.WriteLine("Enter Wall Length");
+        double wallLength =double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Enter Wall Height");
+        double wallHeight = double.Parse(Console.ReadLine());
+
+        double area = wallHeight * wallLength;
+
+
+        Console.WriteLine($"Wall Name: {wallName}");
+        Console.WriteLine($"Wall Length: {wallLength}");
+        Console.WriteLine($"Wall Height: {wallHeight}");
+        Console.WriteLine($"Wall Area: {area}");
+
+
+
+
+        if (area<10)
+        {
+            Console.WriteLine("Small Wall");
+        }
+        if(10<=area && area<30)
+        {
+            Console.WriteLine("Medium Wall");
+        }
+        if (area>30)
+        {
+            Console.WriteLine("Large Wall");
+        }
+
+
+
+
+
+
+    }
+}
