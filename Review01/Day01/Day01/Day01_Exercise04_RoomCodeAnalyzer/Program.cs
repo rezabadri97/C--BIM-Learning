da447d1e9a2649f265b33489738355dcf9d1ed05
Rezabadri97i <Rezabadri97@gmail.com>
2026-06-08 07:19:04 +0330

    Implement BIM door width validation report

--- Review01/Day01/Day01/Day01_Exercise04_RoomCodeAnalyzer/Program.cs
+++ Review01/Day01/Day01/Day01_Exercise04_RoomCodeAnalyzer/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BimDeveloperExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] roomCodes = new string[5];
+
+            int architecturalCount = 0;
+            int mechanicalCount = 0;
+            int electricalCount = 0;
+            int serviceCount = 0;
+            int unknownCount = 0;
+
+            for (int i = 0; i < roomCodes.Length; i++)
+            {
+                Console.WriteLine($"Enter room code #{i + 1}:");
+                string inputCode = Console.ReadLine();
+
+                string cleanCode = inputCode.Trim().ToUpper();
+
+                roomCodes[i] = cleanCode;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Clean Room Codes:");
+
+            foreach (string code in roomCodes)
+            {
+                Console.WriteLine(code);
+
+                if (code.StartsWith("A"))
+                {
+                    architecturalCount++;
+                }
+                else if (code.StartsWith("M"))
+                {
+                    mechanicalCount++;
+                }
+                else if (code.StartsWith("E"))
+                {
+                    electricalCount++;
+                }
+                else if (code.StartsWith("S"))
+                {
+                    serviceCount++;
+                }
+                else
+                {
+                    unknownCount++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=========== Room Code Report ===========");
+            Console.WriteLine($"Total Rooms: {roomCodes.Length}");
+            Console.WriteLine($"Architectural Rooms: {architecturalCount}");
+            Console.WriteLine($"Mechanical Rooms: {mechanicalCount}");
+            Console.WriteLine($"Electrical Rooms: {electricalCount}");
+            Console.WriteLine($"Service Rooms: {serviceCount}");
+            Console.WriteLine($"Unknown Rooms: {unknownCount}");
+        }
+    }
+}
---
+++ Review01/Day01/Day01/Day01_Exercise05_DoorWidthStandardChecker/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BimDeveloperExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double minimumStandardWidth = 0.9;
+
+            List<string> doorCodes = new List<string>();
+            List<double> doorWidths = new List<double>();
+            List<string> notStandardDoors = new List<string>();
+
+            int standardDoorCount = 0;
+            int notStandardDoorCount = 0;
+            double totalWidth = 0;
+
+            Console.WriteLine("Enter number of doors:");
+            int doorCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= doorCount; i++)
+            {
+                Console.WriteLine($"Enter door code #{i}:");
+                string doorCode = Console.ReadLine();
+
+                Console.WriteLine($"Enter door width #{i} in meters:");
+                double doorWidth = double.Parse(Console.ReadLine());
+
+                doorCodes.Add(doorCode);
+                doorWidths.Add(doorWidth);
+
+                totalWidth += doorWidth;
+
+                if (doorWidth >= minimumStandardWidth)
+                {
+                    standardDoorCount++;
+                    Console.WriteLine($"Door {doorCode} is Standard");
+                }
+                else
+                {
+                    notStandardDoorCount++;
+                    notStandardDoors.Add(doorCode);
+                    Console.WriteLine($"Door {doorCode} is Not Standard");
+                }
+
+                Console.WriteLine();
+            }
+
+            double averageWidth = totalWidth / doorCount;
+
+            Console.WriteLine("=========== Door Width Report ===========");
+            Console.WriteLine($"Total Doors: {doorCount}");
+            Console.WriteLine($"Standard Doors: {standardDoorCount}");
+            Console.WriteLine($"Not Standard Doors: {notStandardDoorCount}");
+            Console.WriteLine($"Total Width: {totalWidth}");
+            Console.WriteLine($"Average Width: {averageWidth}");
+
+            Console.WriteLine();
+            Console.WriteLine("Not Standard Door List:");
+
+            if (notStandardDoors.Count == 0)
+            {
+                Console.WriteLine("All doors are standard.");
+            }
+            else
+            {
+                foreach (string doorCode in notStandardDoors)
+                {
+                    Console.WriteLine(doorCode);
+                }
+            }
+        }
+    }
+}
